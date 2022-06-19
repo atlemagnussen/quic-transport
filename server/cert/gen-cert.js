525a15b8ebe6bf08bd084a5c588260c171367cf5
Atle Magnussen <atlemagnussen@gmail.com>
2022-06-19 20:48:23 +0200

    opts

--- server/cert/gen-cert.js
+++ server/cert/gen-cert.js
@@ -1,9 +1,21 @@
 import { generateWebTransportCertificate} from "./cert.js"
 
+const attrs = [
+    { shortName: 'C', value: 'DE' },
+    { shortName: 'ST', value: 'Berlin' },
+    { shortName: 'L', value: 'Berlin' },
+    { shortName: 'O', value: 'WebTransport Test Server' },
+    { shortName: 'CN', value: '127.0.0.1' }
+]
+
+const options = {
+    days: 13
+}
+
 async function start() {
     console.log("start")
-    await generateWebTransportCertificate()
-    console.log("end")
+    const cert = await generateWebTransportCertificate(attrs, options)
+    console.log("cert", cert)
 }
 
 start().then(() => {
--- server/src/server.js
+++ server/src/server.js
@@ -1,23 +1,47 @@
 
-import { readFileSync } from 'fs'
+// import { readFileSync } from 'fs'
 import { Http3Server } from "@fails-components/webtransport"
-// import { Http3Server } from '../src/webtransport.js'
 import { runEchoServer } from './testsuite.js'
 
-const crt = readFileSync('cert/out/leaf_cert.pem')
-const privKey = readFileSync('cert/out/leaf_cert.key')
+import { generateWebTransportCertificate } from "./cert.js"
 
-try {
-  const http3server = new Http3Server({
-    port: 8080,
-    host: '0.0.0.0',
-    secret: 'mysecret',
-    cert: crt,
-    privKey: privKey
-  })
-  
-  runEchoServer(http3server)
-  http3server.startServer() // you can call destroy to remove the server
-} catch (error) {
-  console.log('http3error', error)
+const attrs = [
+    { shortName: 'C', value: 'DE' },
+    { shortName: 'ST', value: 'Berlin' },
+    { shortName: 'L', value: 'Berlin' },
+    { shortName: 'O', value: 'WebTransport Test Server' },
+    { shortName: 'CN', value: '127.0.0.1' }
+]
+
+const options = {
+    days: 13
 }
+
+//const cert = readFileSync('cert/out/leaf_cert.pem')
+//const privKey = readFileSync('cert/out/leaf_cert.key')
+
+async function run() {
+
+    const certificate = generateWebTransportCertificate(attrs, options)
+    const cert = certificate.cert, // unclear if it is the correct format
+    const privKey = certificate.private
+
+    try {
+        const http3server = new Http3Server({
+            port: 8080,
+            host: '0.0.0.0',
+            secret: 'mysecret',
+            cert,
+            privKey
+        })
+
+        runEchoServer(http3server)
+        http3server.startServer() // you can call destroy to remove the server
+    } catch (error) {
+        console.log('http3error', error)
+    }
+}
+
+run().catch(err => {
+    console.error("general error", err)
+})
